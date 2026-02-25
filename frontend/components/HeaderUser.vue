8466546d10e177cbc91d6617d1b56f4ff2c7aff2
a24ikematgar <a24ikematgar@inspedralbes.cat>
2026-02-25 11:59:51 +0100

    He solucionat el error del header que no redirigia ve a la homePage

--- frontend/components/HeaderUser.vue
+++ frontend/components/HeaderUser.vue
@@ -2,14 +2,14 @@
   <header class="w-full p-3">
     <nav class="w-full flex items-center justify-between px-4">
       
-      <NuxtLink to="/" class="brand">
+      <NuxtLink to="/HomePage" class="brand">
         <img :src="logo" alt="Loopy logo" class="logo-img"/>
         <span class="app-name">Loopy</span>
       </NuxtLink>
 
       <ul class="flex-1 flex items-center justify-center space-x-6 list-none m-0 p-0">
         <li>
-          <NuxtLink to="/" class="nav-link">Home</NuxtLink>
+          <NuxtLink to="/HomePage" class="nav-link">Home</NuxtLink>
         </li>
         <li>
           <NuxtLink to="/habits" class="nav-link">Crear</NuxtLink>

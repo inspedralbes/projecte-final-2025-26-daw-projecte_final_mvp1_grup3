b40fd6ad4051561b7e2f0dd951ec190ee573fd89
Iker <a24ikematgar@inspedralbes.cat>
2026-03-09 22:15:54 +0100

    hola

--- backend-laravel/config/cors.php
+++ backend-laravel/config/cors.php
@@ -3,7 +3,10 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'auth/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [
+        env('FRONTEND_URL', 'http://localhost:3000'),
+        'https://loopy.daw.inspedralbes.cat'
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
